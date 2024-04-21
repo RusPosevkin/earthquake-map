a37b7f000bb6003840f1402d59562362931f5a87
Ruslan Posevkin <rus.posevkin@gmail.com>
2024-04-21 20:50:32 +0100

    Update UI of items list

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -18,11 +18,14 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="list-items">
-    <h1>Made By Getters</h1>
-    <div v-for="earthquake in getFilteredEarthquakes" :key="earthquake.id">
-      {{ earthquake.place }} {{ earthquake.magnitude }} {{ earthquake.coordinates }}
-    </div>
+  <div class="list-items-wrapper">
+    <h1 class="title">Last month earthquakes</h1>
+    <ul class="list" v-if="getFilteredEarthquakes.length > 0">
+      <li class="list-item" v-for="earthquake in getFilteredEarthquakes" :key="earthquake.id">
+        <h2 class="subtitle">{{ earthquake.place }}</h2>
+        <p>Magnitude: <span class="magnitude">{{ earthquake.magnitude }}</span></p>
+      </li>
+    </ul>
     <!-- <h1>Made By Actions</h1>
     <div v-for="earthquake in earthquakes" :key="earthquake.id">
       {{ earthquake.place }} {{ earthquake.magnitude }} {{ earthquake.coordinates }}
@@ -31,11 +34,41 @@ onMounted(() => {
 </template>
 
 <style>
-.list-items {
-  flex: 1;
-  display: flex;
-  /* position: relative; */
-  width: 50px;
-  overflow: hidden;
+.list-items-wrapper {
+  background-color: #94a9b3;
+  color: black;
+  min-width: 300px;
+  max-width: 35%;
+  overflow-y: scroll;
+}
+
+.title {
+  text-align: center;
+  margin-bottom: 10px;
+  font-size: 20px;
+}
+
+.subtitle {
+  font-size: 14px;
+  font-weight: bold;
+}
+
+.magnitude {
+  font-weight: bold;
+  color: rgb(210, 25, 25);
+}
+
+.list {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.list-item {
+  background-color: #a3ccff;
+  padding: 5px;
+  cursor: pointer;
+  margin: 0 10px 10px;
+  border-radius: 5px;
 }
 </style>
